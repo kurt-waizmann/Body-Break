8f5a27ab72ca94124dc3654c2ff028aaaa7170bf
“Ben <123bfran@gmail.com>
2022-07-29 15:58:46 -0400

    TODO List

---
+++ TODO.js
@@ -0,0 +1,61 @@
+//TODO DEFAULT MEETING
+//? CHECK-IN 10AM EVERYDAY
+
+//TODO MVP
+
+//* TODO FRONTEND
+//TODOView all items in the database.
+//TODOPurchase items that are in stock.
+//TODOView a cart containing the items they intend to purchase.
+//TODOEdit cart before completing the purchase.
+
+//*BACKEND
+// The Node server should
+
+//TODO Be RESTful (use the right method for the right job).
+//TODO Provide the FE with the required data in a clear and organized way.
+//TODO Update the database as users make purchases.
+
+//* STRETCH GOALS
+//TODO View the app on a variety of devices. app should look decent an all screen sizes. (FE)
+//TODO User can signin via Google Auth, or Autho0 (BE, FE)
+//TODO User can search the store for item(s) based on various criteria (up to you) (BE, FE)
+
+//*CODE REQUIREMENTS
+//* No console logs! Unless they are part of a catch (logging an error).
+//* Comments! Comments everywhere!
+//* Every function should have a comment above it that briefly explains what it does.
+//* Any block of code (algorithm) should have comments above it that briefly explain what it's doing.
+//* Variables should have comments explaing what they're for. Especially if there are many with similar names or the name isn't very clear.
+//* Organization! No file should be several hundred lines long! Split your code up into different files.
+//* No bugs! (In a perfect world this would be possible. For now, strive to minimize them).
+
+//*KICKOFF MEETING
+
+//*PROJECT MANAGER NOTES
+//TODO everyone should do a commit, push, pull before 5pm so Gina can help us.
+//TODO create a branch called test, and do a commit, push, and merge, and then pull the in the result.
+//TODO assign tasks to specific people.
+
+//*BRAINSTORMING
+//* Take a look at the data, What do we have to show?
+//* How can we make the most of the provided data?
+//* Ideas for store functionality
+//* What do we want users to be able to do?
+//* Look and feel
+//* Any real-life e-comm sites that inspires us?
+
+//* Division of labor
+
+//* Begin to break down the project into tasks, and then into subtasks, and maybe even subsubtasks. Breaking things down into the smallest task possible will improve the collaborative effort of each individual team member.
+
+//* Move all tasks to a board of some kind. Trello would be the easiest to figure out and use.
+//?* Assign tasks
+
+//* Possible questions (relevance will depend on group size)...
+//* Who is most excited about a particular task?
+//* Do we need a manager for a particular group of tasks? i.e. BE, FE.
+//* Who will coordinate between the BE and FE.
+
+//* TEAM Name
+//TEAM NAME IS Watch Out
