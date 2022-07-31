2bd3010818e526fa5dceef2880069d57847c56b8
Philip Willems <philw_138@hotmail.com>
2022-07-31 10:15:22 -0400

    Context,Allitemspage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,18 +5,20 @@ import Fitness from "./Fitness";
 import Homepage from "./Homepage";
 import Lifestyle from "./Lifestyle";
 import Medical from "./Medical";
+import AllItems from "./AllItems";
 
 const App = () => {
   return (
     <>
       <Router>
-      <GlobalStyles />
+        <GlobalStyles />
         <Routes>
           <Route exact path="/" element={<Homepage />} />
           <Route exact path="/fitness" element={<Fitness />} />
           <Route exact path="/medical" element={<Medical />} />
           <Route exact path="/lifestyle" element={<Lifestyle />} />
           <Route exact path="/entertainment" element={<Entertainment />} />
+          <Route exact path="/allitems" element={<AllItems />} />
         </Routes>
       </Router>
     </>
