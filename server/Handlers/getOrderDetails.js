c71ff13b04be16d3d130d81c3fd6e9acfdb40603
Kurt Waizmann <kurtwaizmann@gmail.com>
2022-08-02 11:02:04 -0400

    created getOderDetails handler

--- server/Handlers/getOrderDetails.js
+++ server/Handlers/getOrderDetails.js
@@ -1,7 +1,38 @@
 
+const { creatClient } = require("./utils");
+
+const client = creatClient();
+const dbName = "Group-Project-Watchout";
 
 
-const getOrderDetails = (req, res) => {}
+const getOrderDetails = async (req, res) => {
+    try {        
+        // connect to the client
+        await client.connect();
+
+        // connect to the database 
+        const db = client.db(dbName);
+        console.log("connected!");
+
+        // grabbing from the collection
+        const result = (await db.collection("orders").findOne( {}, { sort: { $natural : -1 }}));
+
+        // response
+        result
+        ? res.status(200).json({ status:200, message: "Success!", data: result})
+        : res.status(404).json({ status:404, message: "Not found", data: result});
+
+    // catch any errors and return info/message
+    } catch (err) {
+        console.log(err.stack);
+        res.status(500).json({ status: 500, data: req.body, message: err.message });
+
+    // close the connection to the database server
+    } finally {
+        client.close();
+        console.log("disconnected!");
+    }
+}
 
 
 module.exports = { getOrderDetails};
