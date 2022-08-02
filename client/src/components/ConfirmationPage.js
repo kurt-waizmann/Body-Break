5078651378bd1cdc0688eee62980c20b616712d1
Kurt Waizmann <kurtwaizmann@gmail.com>
2022-08-02 12:02:58 -0400

    finished confirmation page

--- client/src/components/ConfirmationPage.js
+++ client/src/components/ConfirmationPage.js
@@ -7,23 +7,40 @@ import Header from "./Header";
 const ConfirmationPage = () => {
     const [confirmation, setConfirmation] = useState(null);
 
-    // useEffect(() => {
-    //     fetch(`/api/order/details`)
-    //     .then((res) => res.json())
-    //     .then((data) => {
-    //       setConfirmation(data.data)
-    //     })
-    // },[])
+    useEffect(() => {
+        fetch(`/api/order/details`)
+        .then((res) => res.json())
+        .then((data) => {
+        setConfirmation(data.data)
+        })
+    },[])
 
+    console.log(confirmation)
     return (
         <>
         <Wrapper>
             <Header />
             <InnerWrap>
-                <Summary>
-                    <Title>Thank your for your purchase!</Title>
-                    <div>Confimation Number: "confirmation" variable goes here </div>
-                </Summary>
+                    {confirmation && <>
+                    <Title>Thank you for your purchase!</Title>
+                    <Summary>
+                        <OrderSummary>
+                            
+                            <Order>
+                            <div>Confimation Number: <Details>{confirmation._id}</Details></div>
+                            <span>Number of Items: <Details>{confirmation.items.length}</Details> </span>
+                            <span>Cost: <Details>{confirmation.cost}</Details> </span>
+                            </Order>
+                            <Line/>
+                            <Order>
+                            <div>Name: <Details>{confirmation.firstName} {confirmation.lastName}</Details> </div>
+                            <div>Address: <Details>{confirmation.address}</Details> </div>
+                            <div>Email: <Details>{confirmation.email}</Details> </div>
+                            </Order>
+                        </OrderSummary>
+                    </Summary>
+                    <Button>Return to Homepage</Button>
+                    </>}
             </InnerWrap>
         </Wrapper>
         </>
@@ -35,7 +52,6 @@ export default ConfirmationPage;
 
 const Wrapper = styled.div`
     font-family: "Poppins", sans-serif;
-    font-weight: bold;
     background-color: #1c1b1b;
     color: white;
     height: 100%;
@@ -51,7 +67,6 @@ const InnerWrap = styled.div`
 `
 
 const Summary = styled.div`
-    width: 50%;
     border-radius: 3%;
     background-color: #313131;
     color: white;
@@ -59,39 +74,58 @@ const Summary = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
+    align-items: center;
     gap: 30px;
 `
 const Title = styled.div`
     font-size: 30px;
+    color: #04d9ff;
+    margin-bottom: 20px;
+`
+const OrderSummary = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`
+
+const Order = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+`
+
+const Details = styled.span`
+    color:#04d9ff;
+    margin: 0 4px 0 4px;
+`
+
+const Line = styled.div`
+    border-bottom: 2px solid #04d9ff;
 `
 
 const Button = styled.button`
-    color: #04d9ff;
+    background-color: inherit;
+    border: none;
+    color:#878787;
+    font-size: 15px;
     height: 40px;
-    width: 150px;
-    border: 1px solid;
-    border-color: #04d9ff;
-    background-color: black;
-    border-radius: 5px;
-    :before {
-    pointer-events: none;
+    margin: 20px;
+    cursor: pointer;
+    position: relative;
+    :after {
     content: "";
     position: absolute;
-    background: #04d9ff;
-    top: 15%;
-    left: 72%;
-    right: 0;
-    height: 20px;
-    width: 30px;
-    transform: perspective(1em) rotateX(40deg) scale(0.5, 0.6);
-    filter: blur(1em);
-    }
-    :hover {
-    cursor: pointer;
-    color: black;
-    background-color: #04d9ff;
-    transition-timing-function: ease-in-out;
-    transition-duration: 400ms;
-    }
-
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 2px;
+    border-bottom: 1px solid #04d9ff;
+    transform: scaleX(0);
+    transform-origin: right;
+    transition: transform 400ms ease-in-out;
+  }
+  :hover::after {
+    transform: scaleX(1);
+    transform-origin: left;
+  }
 `
