effe54075e437b56937125225af3409852feb7e4
“Ben <123bfran@gmail.com>
2022-08-03 17:10:17 -0400

    Changed Footer Size

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,9 +5,7 @@ import { FaAngleUp } from "react-icons/fa";
 const Footer = () => {
   return (
     <>
-      <Wrapper>
-        <TopBox></TopBox>
-      </Wrapper>
+      <Wrapper></Wrapper>
     </>
   );
 };
@@ -15,15 +13,9 @@ const Wrapper = styled.footer`
   display: flex;
   position: relative;
   flex-direction: column;
-  height: 200px;
+  height: 88px;
   /* padding: 80px 0px; */
-  background: #191818;
-`;
-const TopBox = styled.div`
-  background: #1c1b1b;
-  color: white;
-  width: 100%;
-  height: 50px;
+  background: #030303;
 `;
 
 export default Footer;
